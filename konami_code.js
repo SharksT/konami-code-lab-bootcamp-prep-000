2d5c06d22245dd93cc5e476b4ac1ef51f91cfcf4
Felipe Passos <felipekaires@hotmail.com>
2017-10-24 18:26:23 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
-function init(onKeyDownHandler) {
+function init(function(event) {
   // Write your JavaScript code inside the init() function
   function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
