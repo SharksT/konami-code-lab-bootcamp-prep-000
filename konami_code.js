b14172107605c06ad2987866767a2ae689df35ee
Felipe Passos <felipekaires@hotmail.com>
2017-10-24 18:34:00 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -6,26 +6,26 @@ function init() {
   const input = document.querySelector('input')
 
   input.addEventListener('keydown',function(e)) {
-    
-  }
-  
-  function onKeyDownHandler(e) {
+    function onKeyDownHandler(e) {
 
-  const key = parseInt(e.detail || e.which);
+    const key = parseInt(e.detail || e.which);
 
-  if (key === code[index]) {
-    index++;
-    console.log("Teste ");
-    if (index === code.length) {
-      console.log("Teste 2");
-      alert("Hurray!");
+    if (key === code[index]) {
+      index++;
+      console.log("Teste ");
+      if (index === code.length) {
+        console.log("Teste 2");
+        alert("Hurray!");
 
+        index = 0;
+      }
+    }
+    else {
       index = 0;
     }
   }
-  else {
-    index = 0;
   }
-}
+
+  
 
 }
