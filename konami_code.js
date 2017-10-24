82894c7a3a3cb57cad506e7fe2b3eeb3447c722d
Felipe Passos <felipekaires@hotmail.com>
2017-10-24 19:26:47 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
 
-  document.addEventListener("keydown",function onKeyDownHandler(e) {
+  document.body.addEventListener("keydown",function onKeyDownHandler(e) {
 
 
       const key = parseInt(e.which);
